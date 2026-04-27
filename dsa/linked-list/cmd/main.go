c583e813141d16db2e7530addb240790400ec601
Radheshyam Tiwari <radhe.tiwari@ibm.com>
2026-04-27 21:00:30 +0530

    Add linked list traversal example

---
+++ dsa/linked-list/cmd/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+type Node struct{
+	Value int
+	Next *Node
+}
+
+func CountNodes(head *Node) int{
+	count := 0
+	current := head
+
+	for current !=nil{
+		count++
+		current= current.Next
+	}
+	return count
+}
+
+func main(){
+	head := &Node{Value: 10}
+	second := &Node{Value: 20}
+	third := &Node{Value: 30}
+
+	head.Next= second
+	second.Next=third
+	current := head
+	result := CountNodes(head)
+
+	fmt.Println("Total nodes:", result)
+
+	for current != nil {
+		fmt.Println(current.Value)
+		current = current.Next
+	}
+}
