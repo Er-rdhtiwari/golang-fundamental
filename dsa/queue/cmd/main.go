9d97f4fba42743f52b2d846fdde2269d065d0839
Radheshyam Tiwari <radhe.tiwari@ibm.com>
2026-04-27 20:08:55 +0530

    Add queue implementation example

---
+++ dsa/queue/cmd/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+
+)
+
+type Queue struct{
+	items []string
+}
+
+func (q *Queue) Enqueue(item string){
+	q.items = append(q.items, item)
+}
+
+func (q *Queue) Dequeue()(string, error){
+	if q.IsEmpty(){
+		return "" , fmt.Errorf("queue is empty")
+	}
+	firstItem := q.items[0]
+	q.items = q.items[1:]
+	return firstItem, nil
+
+}
+
+func (q *Queue) IsEmpty() bool {
+	return len(q.items) == 0
+}
+
+func main(){
+	queue := Queue{}
+	queue.Enqueue("pr-event")
+	queue.Enqueue("cd-event")
+	queue.Enqueue("job-event")
+
+	for !queue.IsEmpty(){
+		event, err := queue.Dequeue()
+		if err!=nil{
+			fmt.Println("error: ", err)
+			return
+		}
+		fmt.Println("Processing:", event)
+	}
+}
